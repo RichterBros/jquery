94ade251fb02e2fcc01db2b12cb9349fffb0ea2b
Student <example@example.com>
2020-01-13 16:06:56 -0800

    create files

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+var firstLast = function(userWord){
+var newWordArray = userWord.split("");
+
+
+var lastLetter = newWordArray.pop().toUpperCase();
+
+
+var firstLetter = newWordArray.shift().toUpperCase();
+
+var middleLetters = newWordArray.join(""); 
+
+var end = firstLetter + middleLetters + lastLetter
+
+return end;
+
+
+
+};
+
+
+
+
+
+$(document).ready(function(){
+
+  var userword = prompt("enter a word")
+  
+  
+  alert(firstLast(userword));
+});
+
+
+
