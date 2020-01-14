c6f1d8063f489ddb2c102b9911efd3eebc1d5937
Student <example@example.com>
2020-01-13 16:40:47 -0800

    start aditional project

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,49 @@
 
-var firstLast = function(userWord){
-var newWordArray = userWord.split("");
+// var firstLast = function(userWord){
+// var newWordArray = userWord.split("");
 
 
-var lastLetter = newWordArray.pop().toUpperCase();
+// var lastLetter = newWordArray.pop().toUpperCase();
 
 
-var firstLetter = newWordArray.shift().toUpperCase();
+// var firstLetter = newWordArray.shift().toUpperCase();
 
-var middleLetters = newWordArray.join(""); 
+// var middleLetters = newWordArray.join(""); 
 
-var end = firstLetter + middleLetters + lastLetter
+// var end = firstLetter + middleLetters + lastLetter
 
-return end;
-
-
-
-};
+// return end;
 
 
+
+// };
+
+
+
+
+
+// $(document).ready(function(){
+
+//   var userword = prompt("enter a word")
+  
+  
+//   alert(firstLast(userword));
+// });
 
 
 
 $(document).ready(function(){
 
-  var userword = prompt("enter a word")
-  
-  
-  alert(firstLast(userword));
-});
+  var userSentence = prompt("enter a sentence");
 
+  var capitol = function(){
+    var firstLetter = userSentence.length(0);
+    return firstLetter
+  }
+
+  console.log(capitol());
+
+
+ });
 
 
